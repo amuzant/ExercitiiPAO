eb0042ab416c22125d80caefa8d18b04102beca1
Andrei Antonescu <aynd3r@gmail.com>
2024-03-26 14:42:44 +0200

    adaugat exercitiul cu regex - lab 5

--- ExercitiiLab5/src/ExercitiuRegex.java
+++ ExercitiiLab5/src/ExercitiuRegex.java
@@ -9,5 +9,25 @@ public class ExercitiuRegex {
         System.out.println("abcba".matches("[abc]{5}"));
         System.out.println("abcbabc".matches("[abc]{5,}"));
         System.out.println("abcbabca".matches("[abc]{5,10}"));
+
+        String string="Odata creat un sir de caractere cu continutul sau nu mai poate fi modificat.";
+        System.out.println(string.length());
+        System.out.println(string.matches("[a-zA-Z]+"));
+        String[] cuvinte=string.split(" ");
+
+        int nrCuvinte = cuvinte.length;
+        System.out.println(nrCuvinte);
+        if (nrCuvinte>1)
+            if (nrCuvinte%2==0)
+                System.out.println("Cuvintele de la mijloc: "+cuvinte[nrCuvinte/2-1]+" "+cuvinte[nrCuvinte/2]);
+            else System.out.println("Cuvantul de la mijloc: "+cuvinte[nrCuvinte/2]);
+        else if(nrCuvinte==1)
+            System.out.println("Cuvantul de la mijloc (singurul): "+cuvinte[0]);
+        else System.out.println("Stringul nu contine cuvinte");
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=string.length()-1;i>=0;i--)
+            sb.append(string.charAt(i));
+        System.out.println(sb);
     }
 }
